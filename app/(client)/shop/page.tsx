25a32fea72193bf3d39c69a68b6b77336099061e
minh le <117999133+sixxson@users.noreply.github.com>
2025-05-14 16:47:16 +0700

    fix bug searchParams

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -1,14 +1,18 @@
 import { getAllBrands, getCategories } from "@/sanity/queries";
-import React from "react";
+import React, { Suspense } from "react";
 import Shop from "@/components/Shop/shop";
 
 export default async function ShopPage() {
   const categories = await getCategories();
   const brands = await getAllBrands();
+
   return (
     <div className="bg-none">
-      <Shop categories={categories} brands={brands} />
+      <Suspense fallback={<div>Loading shop...</div>}>
+        <Shop categories={categories} brands={brands} />
+      </Suspense>
     </div>
   );
-};
+}
+
 
